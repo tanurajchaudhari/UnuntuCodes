db43bdc3ae2443f439dcaab496ff4a158636a68c
tanuraj <tanuraj169179@gmail.com>
2024-08-28 11:37:40 +0530

    dart

---
+++ DARTONLINUX/prg1.dart
@@ -0,0 +1,99 @@
+/*class Player{
+
+  int ? jerNo;
+  String?pName;
+  double?Sal;
+  
+  Player(int jerNo,String pName,double Sal){
+      this.jerNo=jerNo;
+
+
+
+      print (jerNo);
+       print(pName);
+       print(Sal);
+}
+ void playerInfo(){
+
+   print (jerNo);
+
+   print (pName);
+
+   print (Sal);
+
+
+}
+
+}
+void main(){
+
+   Player obj=new Player(18,"virat",7.0);
+   obj.playerInfo();
+
+   
+
+
+}*/
+
+
+class Player{
+
+  int ? jerNo;
+  String?pName;
+  double?Sal;
+
+  Player(this .jerNo,this. pName,this. Sal){
+      
+
+        
+
+     
+}
+ void playerInfo(){
+
+   print (jerNo);
+
+   print (pName);
+
+   print (Sal);
+
+
+}
+
+}
+void main(){
+
+   Player obj=new Player(18,"virat",7.0);
+   obj.playerInfo();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
